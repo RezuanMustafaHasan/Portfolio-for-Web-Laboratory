c990977eb864dd075f2f0886abc1a5991083e84c
Rezuan <hasanideal2002@gmail.com>
2025-08-25 17:38:25 +0600

    Configuring PROFILE Variable2

--- inc/head.php
+++ inc/head.php
@@ -1,15 +1,1 @@
-<?php
-// ---------- Basic Profile Config ----------
-$PROFILE = [
-  "name" => "Rezuan Mustafa Hasan",
-  "role" => "CS Undergrad • Full‑Stack & Problem Solving",
-  "tagline" => "I build practical products and love efficient code.",
-  "email" => "hasanideal2002@gmail.com",
-  "phone" => "(+880) 1748855331",
-  "location" => "Bangladesh",
-  "github_user" => "RezuanMustafaHasan",
-  "github_url" => "https://github.com/RezuanMustafaHasan",
-  "linkedin_url" => "https://www.linkedin.com/in/rezuan-mustafa-hasan-ab0ab9242/",
-  "resume_path" => "assets/cv.pdf"
-];
 
